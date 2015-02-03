922b5fca081293c3124551fdb70b105b6aaa2c6f
radarsh <adarsh.ramamurthy@gmail.com>
2015-02-03 17:17:11 +0000

    Set indentation right

--- bashmon.sh
+++ bashmon.sh
@@ -5,10 +5,10 @@ declare jobs="$2"
 declare query="lastBuild/api/json?pretty=true&tree=result,building"
 
 for job in $jobs; do
-	toilet -f standard -t $job
-	tput setaf 2
-	curl -s "$jenkins_host/job/$job/$query" | grep result | awk '{print $3}' | sed 's/[",]//g' | toilet -t -f mini
-	tput sgr0
-	echo 
+    toilet -f standard -t $job
+    tput setaf 2
+    curl -s "$jenkins_host/job/$job/$query" | grep result | awk '{print $3}' | sed 's/[",]//g' | toilet -t -f mini
+    tput sgr0
+    echo 
 done
 
