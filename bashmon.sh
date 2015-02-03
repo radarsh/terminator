54c0647d09160b59037466dd9ba06791ed328067
radarsh <adarsh.ramamurthy@gmail.com>
2015-02-03 15:31:40 +0000

    Got rid of python dependency. Also, it now prints success in green

--- bashmon.sh
+++ bashmon.sh
@@ -2,8 +2,13 @@
 
 declare jenkins_host="$1"
 declare jobs="$2"
+declare query="lastBuild/api/json?pretty=true&tree=result,building"
 
-echo Jenkins host $jenkins_host
-echo Job list $jobs
+for job in $jobs; do
+	toilet -f standard -t $job
+	tput setaf 2
+	curl -s "$jenkins_host/job/$job/$query" | grep result | awk '{print $3}' | sed 's/[",]//g' | toilet -t -f mini
+	tput sgr0
+	echo 
+done
 
-for job in $jobs; do toilet -f standard -t $job; echo $jenkins_host/job/$job/adfdsaf ;curl $jenkins_host/job/$job/lastBuild/api/json 2> /dev/null | python -m json.tool | grep result | awk '{print $2}' | sed 's/[",]//g' | toilet -t -f mini -F gay ; echo; done
